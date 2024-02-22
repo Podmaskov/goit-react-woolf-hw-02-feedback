e7cafc5a22061f68cc2055c482c21f0c33f22072
Podmaskov <podmaskov@gmail.com>
2024-02-22 12:31:50 +0100

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-import { FeedbackOptions } from './FeedbackOptions';
-import { Section } from './Section';
-import { Statistics } from './Statistics';
-import { Notification } from './Notification';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
 import styles from './styles.module.css';
 
 const OPTIONS = ['Good', 'Neutral', 'Bad'];
